5409597fe8b7ab001f299bc4ea3bef19accde5b1
Gulilil <juan.csantoso@gmail.com>
2025-01-06 16:07:29 +0700

    feat: finishing import and processing data

--- src/import.py
+++ src/import.py
@@ -1,8 +1,13 @@
 from dotenv import load_dotenv
 import os
 import psycopg2
+import pandas as pd
+import time
+from queries import CREATE_TABLE_INVENTORY_DETAILS, CREATE_TABLE_INVENTORY_SALES
 
 load_dotenv()
+
+DATA_DIR = os.path.join(os.getcwd(), "data")
 
 def execute_query(query: str):
   conn = None
@@ -31,3 +36,88 @@ def execute_query(query: str):
 
 def make_string(text: str, quote : str = "\'"):
   return f"{quote}{text}{quote}"
+
+def insert_details(df):
+  i = 0
+  max_batch = 1000
+  while (i <= len(df)):
+    bound = min(len(df), i + max_batch)
+    partial_df = df[i:bound]
+    query = "INSERT INTO inventory_details VALUES \n"
+    for _, row in partial_df.iterrows():
+      row_query = "("
+      row_query += str(row['id']) + ", "
+      row_query += make_string(row['s_id']) + ", "
+      row_query += make_string(row['p_id']) + ", "
+      row_query += make_string(row['category']) + ", "
+      row_query += make_string(row['region']) + ", "
+      row_query += make_string(row['seasonality']) + ", "
+      row_query += make_string(row['vt_start']) + ", "
+      row_query += make_string(row['vt_end']) + ", "
+      row_query += make_string(row['tt_start']) + ", "
+      row_query += make_string(row['tt_end'])
+      row_query += "),\n"
+      query += row_query 
+    query = query[:-2] +";"
+    execute_query(query)
+    print(f"[INSERTED] Insert {i} data")
+    i += max_batch
+    time.sleep(0.1)
+
+def insert_sales(df):
+  i = 0
+  max_batch = 1000
+  while (i <= len(df)):
+    bound = min(len(df), i + max_batch)
+    partial_df = df[i:bound]
+    query = "INSERT INTO inventory_sales VALUES \n"
+    for _, row in partial_df.iterrows():
+      row_query = "("
+      row_query += str(row['id']) + ", "
+      row_query += make_string(row['s_id']) + ", "
+      row_query += make_string(row['p_id']) + ", "
+      row_query += str(row['amount']) + ", "
+      row_query += str(row['units_sold'])+ ", "
+      row_query += str(row['units_ordered']) + ", "
+      row_query += str(row['price']) + ", "
+      row_query += str(row['discount']) + ", "
+      row_query += str(row['refer_id']) + ", "
+      row_query += make_string(row['vt_start']) + ", "
+      row_query += make_string(row['vt_end']) + ", "
+      row_query += make_string(row['tt_start']) + ", "
+      row_query += make_string(row['tt_end'])
+      row_query += "),\n"
+      query += row_query 
+    query = query[:-2] +";"
+    execute_query(query)
+    print(f"[INSERTED] Insert {i} data")
+    i += max_batch
+    time.sleep(0.1)
+
+if __name__ == "__main__":
+  conn = None
+  cur = None
+  try:
+    conn = psycopg2.connect(user=os.getenv("POSTGRE_DB_USER"), 
+                            database=os.getenv("POSTGRE_DB_NAME"),
+                            password=os.getenv("POSTGRE_DB_PASSWORD"),
+                            host=os.getenv("POSTGRE_DB_HOST"),
+                            port=os.getenv("POSTGRE_DB_PORT"))
+    cur = conn.cursor()
+
+    conn.close()
+    cur.close()
+
+    # Create Tables
+    execute_query(CREATE_TABLE_INVENTORY_DETAILS)
+    execute_query(CREATE_TABLE_INVENTORY_SALES)
+    print(f"[SUCCESS] Successfully create tables")
+
+    df_details = pd.read_csv(os.path.join(DATA_DIR, "inventory_details.csv"))
+    df_sales = pd.read_csv(os.path.join(DATA_DIR, "inventory_sales.csv"))
+
+    insert_details(df_details)
+    insert_sales(df_sales)
+
+  except Exception as e: 
+    print(f"[CONNECTION FAILED] Failed to connect to database: {os.getenv('POSTGRE_DB_NAME')} : {e}")
---
+++ src/queries.py
@@ -0,0 +1,31 @@
+CREATE_TABLE_INVENTORY_DETAILS = """CREATE TABLE IF NOT EXISTS inventory_details (
+    id SERIAL PRIMARY KEY,
+    s_id VARCHAR(5) NOT NULL,
+    p_id VARCHAR(5) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    region VARCHAR(10), 
+    seasonality VARCHAR(10),
+    vt_start TIMESTAMP NOT NULL,
+    vt_end TIMESTAMP NOT NULL DEFAULT 'infinity',
+    tt_start TIMESTAMP NOT NULL DEFAULT NOW(),
+    tt_end TIMESTAMP NOT NULL DEFAULT 'infinity'
+);
+"""
+
+CREATE_TABLE_INVENTORY_SALES = """CREATE TABLE IF NOT EXISTS inventory_sales (
+    id SERIAL PRIMARY KEY, 
+    s_id VARCHAR(5) NOT NULL,
+    p_id VARCHAR(5) NOT NULL,
+    amount INT NOT NULL DEFAULT 0,
+    units_sold INT NOT NULL DEFAULT 0, 
+    units_ordered INT NOT NULL DEFAULT 0,
+    price FLOAT NOT NULL,
+    discount INT NOT NULL DEFAULT 0,
+    refer_id INT NOT NULL,
+    vt_start TIMESTAMP NOT NULL,
+    vt_end TIMESTAMP NOT NULL DEFAULT 'infinity',
+    tt_start TIMESTAMP NOT NULL DEFAULT NOW(),
+    tt_end TIMESTAMP NOT NULL DEFAULT 'infinity',
+    FOREIGN KEY (refer_id) REFERENCES inventory_details(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+"""
